343cb9dfc074b0602fa0895ab494eb97ae773de2
fabiomacieira16 <fabiomacieira16@gmail.com>
2020-02-03 22:37:45 -0300

    criar do arquivo ListaProduto

--- ListaProdutos.dart
+++ ListaProdutos.dart
@@ -1,3 +1,20 @@
+import "dart.io";
+
 main(){
-  
+    List<Sring> produtos  = [];
+    bool condicao = true;
+
+    print(==== Adcione um Produto === );
+    strig text = stdin.readLineSinc();
+    if(text == "sair"){
+        print(=== TERMINOU O PROGRAMA == );
+        condicao = false;
+    }else if( text == imprimir ){
+        print(produtos);
+        print("\n");x
+    }else{
+        produtos.add(text);
+        print("\x1b[2J]\x1B[0;0H]");
+    }
+
 }
